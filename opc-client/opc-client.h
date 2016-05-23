8251281a89ff6731edbf89ed2fb5eb8c391c6b8b
Miqueias Miller Penha Dias <miqueiasmiller@gmail.com>
2016-05-23 16:42:37 -0300

    First commit of the opc simple client project.

---
+++ opc-client/opc-client.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifdef OPCCLIENT_EXPORTS
+#define OPCCLIENT_API __declspec(dllexport)
+#else
+#define OPCCLIENT_API __declspec(dllimport)
+#endif
+
+#include <string>
+#include <memory>
+#include <array>
+#include "opcda.h"
+
+// This class is exported from the opc-client.dll
+class OPCCLIENT_API OPCClient {
+private:
+	~OPCClient();
+
+protected:
+	std::unique_ptr<IOPCServer> opcServer;
+	std::wstring const & opcServerName;
+	unsigned int opcDateTime;
+
+	IOPCServer * GetOPCServer(std::wstring const & serverName);
+
+public:
+	OPCClient(std::wstring const & serverName);
+	// TODO: add your methods here.
+};
