043d5d8395cd1d4cc5f8725a9ec8960fb5ed82b3
miqueias <miqueiasmiller@gmail.com>
2016-05-28 02:11:16 -0300

    Criada função para adicionar o grupo de gerenciamento de itens ao servidor.

--- opc-client/opc_client.cpp
+++ opc-client/opc_client.cpp
@@ -4,16 +4,23 @@ namespace opc
 {
 	OPCClient::~OPCClient()
 	{
-		// Releases the OPC Server...
-		if (this->opcServer)
-			this->opcServer->Release();
+		// releases the Item Managemente Group...
+		if (itemMgt.get())
+			itemMgt->ptr->Release();
+
+		// releases the OPC Server...
+		if (opcServer)
+			opcServer->Release();
 	}
 
 
-	OPCClient::OPCClient(wstring const & serverName) : opcServerName(serverName), opcServer(nullptr)
+	OPCClient::OPCClient(wstring const & serverName) : opcServer(nullptr)
 	{
-		// Gets an instance of the IOPCServer and assigns it to the instance variable...
-		this->opcServer = this->GetOPCServer(this->opcServerName);
+		// gets an instance of the IOPCServer and assigns it to the instance variable...
+		opcServer = GetOPCServer(serverName);
+
+		// gets an instance of the managemente group...
+		itemMgt = AddGroupItemManagement(opcServer, 1000);
 	}
 
 
@@ -25,13 +32,36 @@ namespace opc
 		hr = CLSIDFromString(serverName.c_str(), &opcServerId);
 		_ASSERT(hr == NOERROR);
 
-		array<MULTI_QI, 1> instances = {{ &IID_IOPCServer, NULL, 0 }};
+		array<MULTI_QI, 1> instances = { { &IID_IOPCServer, NULL, 0 } };
 
 		hr = CoCreateInstanceEx(opcServerId, NULL, CLSCTX_SERVER, NULL, 1, &instances[0]);
 		_ASSERT(hr == S_OK);
 
-		//this->opcServer.reset(std::move((IOPCServer *)instances[0].pItf));
 		return (IOPCServer *)instances[0].pItf;
+	}
+
+
+	unique_ptr<ItemManagementGroup> OPCClient::AddGroupItemManagement(IOPCServer * opcServer, unsigned long updateRate)
+	{
+		//ItemManagementGroup * group = new ItemManagementGroup();
+		unique_ptr<ItemManagementGroup> group(new ItemManagementGroup());
+
+		HRESULT hr = opcServer->AddGroup(
+			L"ItemManagementGroup",    // szName
+			false, 			               // bActive
+			updateRate, 			         // dwRequestedUpdateRate
+			0, 											   // hClientGroup
+			0, 											   // pTimeBias
+			0, 											   // pPercentDeadband
+			0,                         // dwLCID
+			&group->handle, 				   // phServerGroup
+			&group->updateRate, 		   // pRevisedUpdateRate
+			IID_IOPCItemMgt, 				   // riid
+			(IUnknown **)&group->ptr); // ppUnk
+
+		_ASSERT(hr == S_OK);
+
+		return group;
 	}
 
 
--- opc-client/opc_client.h
+++ opc-client/opc_client.h
@@ -8,22 +8,31 @@
 
 #include <string>
 #include <array>
+#include <memory>
 #include "opcda.h"
 
 using namespace std;
 
 namespace opc
 {
+	struct OPCCLIENT_API ItemManagementGroup {
+		OPCHANDLE handle;
+		IOPCItemMgt * ptr;
+		unsigned long updateRate;
+	};
+
 	class OPCCLIENT_API OPCClient {
 	protected:
 		// a pointer to the IUnknown interface...
 		IOPCServer * opcServer;
 
-		// opc-da server name...
-		wstring const & opcServerName;
+		unique_ptr<ItemManagementGroup> itemMgt;
 
 		// retrieves an IUnknown instance of opc-da server...
 		IOPCServer * GetOPCServer(wstring const & serverName);
+
+		// creates a item management group...
+		unique_ptr<ItemManagementGroup> AddGroupItemManagement(IOPCServer * opcServer, unsigned long updateRate);
 
 	public:
 		// destructor...
